36c006af5eb90147a63072bfbdc95bbc93aaa977
TAISEIKAMIMIZU <tafutoji9121@gmail.com>
2025-03-24 08:42:05 +0900

    Modified A problem in ARC195

--- AtCoder/ARC195_BR/A_Twice_Subsequence_BR/answer.py
+++ AtCoder/ARC195_BR/A_Twice_Subsequence_BR/answer.py
@@ -1,3 +1,7 @@
-N, M = map(int, input().split())
-list_A = "".join(input().split())
-list_B = "".join(input().split())
+def Solution():
+    N, M = map(int, input().split())
+    list_A = "".join(input().split())
+    list_B = "".join(input().split())
+
+
+Solution()
