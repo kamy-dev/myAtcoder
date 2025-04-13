3fe61751e975775230a4e3eb48172830c2b43acc
Taisei_Kamimizu <tafutoji9121@gmail.com>
2025-04-13 18:05:47 +0900

    Added AOJ/ALDS1_2_B

---
+++ AOJ/ALDS1_2_B/answer.py
@@ -0,0 +1,20 @@
+def main():
+    n = int(input())
+    A = [int(i) for i in input().split()]
+
+    cnt = 0
+
+    for i in range(n):
+        minj = i
+        for j in range(i, n):
+            if A[minj] > A[j]:
+                minj = j 
+        A[i], A[minj] = A[minj], A[i]
+        if i != minj:
+            cnt += 1   
+    print(*A)
+    print(cnt)
+    return
+
+if __name__ == '__main__':
+    main()
---
+++ AOJ/ALDS1_2_B/knowledge.md
@@ -0,0 +1,6 @@
+## 復習時に見直すメモ
+
+SelectionSort の問題
+[Link](https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_2_B&lang=ja)
+
+## point
