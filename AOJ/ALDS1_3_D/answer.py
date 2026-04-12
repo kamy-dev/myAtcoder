39c69afa06703cd317beb0167531f3c510db8dc8
Taisei_Kamimizu <tafutoji9121@gmail.com>
2026-04-12 15:03:56 +0900

    20260412

--- AOJ/ALDS1_3_D/answer.py
+++ AOJ/ALDS1_3_D/answer.py
@@ -2,13 +2,21 @@ from collections import deque
 
 if __name__ == "__main__":
     line = list(input())
-    stack = []
+    stack1 = deque()     # '\'の位置
+    stack2 = deque()     # (開始位置、面積)のペア
+    area_sum = 0         #  水たまりの総面積
+    area = 0             # 各水たまりの面積
     for i in range(len(line)):
-        if stack is not None:
-            if line[i] == stack[-1]:
-                stack.append(line[i])
-            else:
-
-        else:
-            stack.append(line[i])
-
+        if line[i] == "\\":
+            stack1.append(i)
+        elif line[i] == "/":
+            if stack1:
+                j = stack1.pop()
+                area = i - j
+                area_sum += i - j
+                while stack2 and stack2[-1][0] > j:
+                    area += stack2.pop()[1]
+                stack2.append((j, area))
+    print(area_sum)
+    areas = [area for _, area in stack2]
+    print(len(stack2), *areas)
